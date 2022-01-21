27aaa7053660e92be5733088a0166764d41f6ded
lukaszpekala <lukasz.pekala.pl@gmail.com>
2022-01-21 11:57:40 +0100

    hi

---
+++ src/main/java/com/example/fbtransactions/FbTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.fbtransactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbTransactionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FbTransactionsApplication.class, args);
+    }
+
+}
